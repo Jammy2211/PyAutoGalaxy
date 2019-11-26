82509fce14fe8159eaf9357d7a412a651f3ef808
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-11-26 19:08:49 +0000

    rleease script

--- release.sh
+++ release.sh
@@ -23,7 +23,9 @@ set +e
 git commit -m "Incremented version number"
 set -e
 
-python setup.py test sdist bdist_wheel
+pytest $p
+
+python setup.py sdist bdist_wheel
 twine upload dist/* --skip-existing --username $PYPI_USERNAME --password $PYPI_PASSWORD
 
 git flow release finish $VERSION
