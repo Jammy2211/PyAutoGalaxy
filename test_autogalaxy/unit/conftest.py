89edce43da3117daa2f83d170a5e093a5d158a8b
Richard <richardhayes777@gmail.com>
2020-10-07 12:47:35 +0100

    use autoarray's mock module where possible

--- test_autogalaxy/unit/conftest.py
+++ test_autogalaxy/unit/conftest.py
@@ -21,52 +21,32 @@ from test_autogalaxy import mock
 
 @pytest.fixture(name="psf_3x3")
 def make_psf_3x3():
-    return aa.Kernel.ones(shape_2d=(3, 3), pixel_scales=(1.0, 1.0))
+    return aa.mock.make_psf_3x3()
 
 
 @pytest.fixture(name="masked_interferometer_7")
-def make_masked_interferometer_7(
-        interferometer_7, visibilities_mask_7x2, mask_7x7, sub_grid_7x7, transformer_7x7_7
-):
-    return aa.MaskedInterferometer(
-        interferometer=interferometer_7,
-        visibilities_mask=visibilities_mask_7x2,
-        real_space_mask=mask_7x7,
-        settings=aa.SettingsMaskedInterferometer(
-            sub_size=1, transformer_class=aa.TransformerDFT
-        ),
-    )
+def make_masked_interferometer_7():
+    return aa.mock.make_masked_interferometer_7()
 
 
 @pytest.fixture(name="rectangular_pixelization_grid_3x3")
-def make_rectangular_pixelization_grid_3x3(grid_7x7):
-    return aa.GridRectangular.overlay_grid(grid=grid_7x7, shape_2d=(3, 3))
+def make_rectangular_pixelization_grid_3x3():
+    return aa.mock.make_rectangular_pixelization_grid_3x3()
 
 
 @pytest.fixture(name="rectangular_inversion_7x7_3x3")
-def make_rectangular_inversion_7x7_3x3(masked_imaging_7x7, rectangular_mapper_7x7_3x3):
-    regularization = aa.reg.Constant(coefficient=1.0)
-
-    return aa.Inversion(
-        masked_dataset=masked_imaging_7x7,
-        mapper=rectangular_mapper_7x7_3x3,
-        regularization=regularization,
-    )
+def make_rectangular_inversion_7x7_3x3():
+    return aa.mock.make_rectangular_inversion_7x7_3x3()
 
 
 @pytest.fixture(name="voronoi_inversion_9_3x3")
-def make_voronoi_inversion_9_3x3(masked_imaging_7x7, voronoi_mapper_9_3x3):
-    regularization = aa.reg.Constant(coefficient=1.0)
-    return aa.Inversion(
-        masked_dataset=masked_imaging_7x7,
-        mapper=voronoi_mapper_9_3x3,
-        regularization=regularization,
-    )
+def make_voronoi_inversion_9_3x3():
+    return aa.mock.make_voronoi_inversion_9_3x3()
 
 
 @pytest.fixture(name="rectangular_mapper_7x7_3x3")
-def make_rectangular_mapper_7x7_3x3(grid_7x7, rectangular_pixelization_grid_3x3):
-    return aa.Mapper(grid=grid_7x7, pixelization_grid=rectangular_pixelization_grid_3x3)
+def make_rectangular_mapper_7x7_3x3():
+    return aa.mock.make_rectangular_mapper_7x7_3x3()
 
 
 @pytest.fixture(name="fit_interferometer_7")
@@ -81,36 +61,22 @@ def make_masked_interferometer_fit_x1_plane_7(masked_interferometer_7):
 
 @pytest.fixture(name="noise_map_7x7")
 def make_noise_map_7x7():
-    return aa.Array.full(fill_value=2.0, shape_2d=(7, 7), pixel_scales=(1.0, 1.0))
+    return aa.mock.make_noise_map_7x7()
 
 
 @pytest.fixture(name="sub_mask_7x7")
 def make_sub_mask_7x7():
-    mask = np.array(
-        [
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, False, False, False, True, True],
-            [True, True, False, False, False, True, True],
-            [True, True, False, False, False, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-        ]
-    )
-
-    return aa.Mask2D.manual(mask=mask, sub_size=2, pixel_scales=(1.0, 1.0))
+    return aa.mock.make_sub_mask_7x7()
 
 
 @pytest.fixture(name="imaging_7x7")
-def make_imaging_7x7(image_7x7, psf_3x3, noise_map_7x7):
-    return aa.Imaging(
-        image=image_7x7, psf=psf_3x3, noise_map=noise_map_7x7, name="mock_imaging_7x7"
-    )
+def make_imaging_7x7():
+    return aa.mock.make_imaging_7x7()
 
 
 @pytest.fixture(name="image_7x7")
 def make_image_7x7():
-    return aa.Array.ones(shape_2d=(7, 7), pixel_scales=(1.0, 1.0))
+    return aa.mock.make_image_7x7()
 
 
 @pytest.fixture(name="masked_imaging_7x7")
@@ -123,70 +89,38 @@ def make_masked_imaging_7x7(imaging_7x7, sub_mask_7x7):
 
 
 @pytest.fixture(name="interferometer_7")
-def make_interferometer_7(visibilities_7x2, noise_map_7x2, uv_wavelengths_7x2):
-    return aa.Interferometer(
-        visibilities=visibilities_7x2,
-        noise_map=noise_map_7x2,
-        uv_wavelengths=uv_wavelengths_7x2,
-    )
+def make_interferometer_7():
+    return aa.mock.make_interferometer_7()
 
 
 @pytest.fixture(name="mask_7x7")
 def make_mask_7x7():
-    mask = np.array(
-        [
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, False, False, False, True, True],
-            [True, True, False, False, False, True, True],
-            [True, True, False, False, False, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-        ]
-    )
-
-    return aa.Mask2D.manual(mask=mask, pixel_scales=(1.0, 1.0), sub_size=1)
+    return aa.mock.make_mask_7x7()
 
 
 @pytest.fixture(name="convolver_7x7")
-def make_convolver_7x7(mask_7x7, blurring_mask_7x7, psf_3x3):
-    return aa.Convolver(mask=mask_7x7, kernel=psf_3x3)
+def make_convolver_7x7():
+    return aa.mock.make_convolver_7x7()
 
 
 @pytest.fixture(name="mask_7x7_1_pix")
 def make_mask_7x7_1_pix():
-    mask = np.array(
-        [
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, True, False, True, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-            [True, True, True, True, True, True, True],
-        ]
-    )
-
-    return aa.Mask2D.manual(mask=mask, pixel_scales=(1.0, 1.0))
+    return aa.mock.make_mask_7x7_1_pix()
 
 
 @pytest.fixture(name="grid_iterate_7x7")
-def make_grid_iterate_7x7(mask_7x7):
-    return aa.GridIterate.from_mask(
-        mask=mask_7x7, fractional_accuracy=0.9999, sub_steps=[2, 4, 8, 16]
-    )
+def make_grid_iterate_7x7():
+    return aa.mock.make_grid_iterate_7x7()
 
 
 @pytest.fixture(name="transformer_7x7_7")
-def make_transformer_7x7_7(uv_wavelengths_7x2, mask_7x7):
-    return aa.TransformerDFT(
-        uv_wavelengths=uv_wavelengths_7x2, real_space_mask=mask_7x7
-    )
+def make_transformer_7x7_7():
+    return aa.mock.make_transformer_7x7_7()
 
 
 @pytest.fixture(name="positions_7x7")
 def make_positions_7x7():
-    return aa.GridCoordinates(coordinates=[[(0.1, 0.1), (0.2, 0.2)], [(0.3, 0.3)]])
+    return aa.mock.make_positions_7x7()
 
 
 @pytest.fixture(name="visibilities_mask_7x2")
@@ -195,13 +129,13 @@ def make_visibilities_mask_7x2():
 
 
 @pytest.fixture(name="grid_7x7")
-def make_grid_7x7(mask_7x7):
-    return aa.Grid.from_mask(mask=mask_7x7)
+def make_grid_7x7():
+    return aa.mock.make_grid_7x7()
 
 
 @pytest.fixture(name="sub_grid_7x7")
-def make_sub_grid_7x7(sub_mask_7x7):
-    return aa.Grid.from_mask(mask=sub_mask_7x7)
+def make_sub_grid_7x7():
+    return aa.mock.make_sub_grid_7x7()
 
 
 @pytest.fixture(name="sub_grid_7x7_simple")
@@ -574,36 +508,13 @@ def make_phase_interferometer_7(mask_7x7):
 
 
 @pytest.fixture(name="voronoi_pixelization_grid_9")
-def make_voronoi_pixelization_grid_9(grid_7x7):
-    grid_9 = aa.Grid.manual_1d(
-        grid=[
-            [0.6, -0.3],
-            [0.5, -0.8],
-            [0.2, 0.1],
-            [0.0, 0.5],
-            [-0.3, -0.8],
-            [-0.6, -0.5],
-            [-0.4, -1.1],
-            [-1.2, 0.8],
-            [-1.5, 0.9],
-        ],
-        shape_2d=(3, 3),
-        pixel_scales=1.0,
-    )
-    return aa.GridVoronoi(
-        grid=grid_9,
-        nearest_pixelization_1d_index_for_mask_1d_index=np.zeros(
-            shape=grid_7x7.shape_1d, dtype="int"
-        ),
-    )
+def make_voronoi_pixelization_grid_9():
+    return aa.mock.make_voronoi_pixelization_grid_9()
 
 
 @pytest.fixture(name="voronoi_mapper_9_3x3")
-def make_voronoi_mapper_9_3x3(grid_7x7, voronoi_pixelization_grid_9):
-    return aa.Mapper(grid=grid_7x7, pixelization_grid=voronoi_pixelization_grid_9)
-
-
-# PLOTTING #
+def make_voronoi_mapper_9_3x3():
+    return aa.mock.make_voronoi_mapper_9_3x3()
 
 
 @pytest.fixture(name="include_all")
