890403950a8069406093ea3cfbc3d1e6125c8016
James Nightingale <james.w.nightingale@durham.ac.uk>
2020-12-20 13:41:23 +0000

    requirements

--- test_autogalaxy/integration/tests/imaging/light/mock_nlo/lens_x2_light__separate.py
+++
@@ -1,9 +0,0 @@
-from test_autogalaxy.integration.tests.imaging.galaxy_x1 import (
-    galaxy_x2__sersics__separate,
-)
-from test_autogalaxy.integration.tests.imaging.runner import run_a_mock
-
-
-class TestCase:
-    def _test__galaxy_x2__sersics__separate(self):
-        run_a_mock(galaxy_x2__sersics__separate)
