fb2831f855dadbeb9727011628ebf3b9a6e71d94
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-03-06 17:42:47 +0000

    AnalysisInterferometer added

--- autogalaxy/analysis/analysis.py
+++ autogalaxy/analysis/analysis.py
@@ -234,3 +234,162 @@ class AnalysisImaging(AnalysisDataset):
         return res.ResultDataset(
             samples=samples, model=model, analysis=self, search=search
         )
+
+
+class AnalysisInterferometer(AnalysisDataset):
+
+    def __init__(
+        self, interferometer, cosmology, results=None,         settings_pixelization=pix.SettingsPixelization(),
+        settings_inversion=inv.SettingsInversion(),
+        preloads=None,
+    ):
+
+        super(Analysis, self).__init__(
+            dataset=interferometer,
+            cosmology=cosmology,
+            results=results,
+            settings_pixelization=settings_pixelization,
+            settings_inversion=settings_inversion,
+            preloads=preloads,
+        )
+
+        result = res.last_result_with_use_as_hyper_dataset(results=results)
+
+        if result is not None:
+
+            self.hyper_galaxy_visibilities_path_dict = (
+                result.hyper_galaxy_visibilities_path_dict
+            )
+
+            self.hyper_model_visibilities = result.hyper_model_visibilities
+
+        else:
+
+            self.hyper_galaxy_visibilities_path_dict = None
+            self.hyper_model_visibilities = None
+
+
+    @property
+    def interferometer(self):
+        return self.dataset
+
+    def log_likelihood_function(self, instance):
+        """
+        Determine the fit of a lens galaxy and source galaxy to the interferometer in this lens.
+
+        Parameters
+        ----------
+        instance
+            A model instance with attributes
+
+        Returns
+        -------
+        fit : Fit
+            A fractional value indicating how well this model fit and the model interferometer itself
+        """
+
+        self.associate_hyper_images(instance=instance)
+        plane = self.plane_for_instance(instance=instance)
+
+        hyper_background_noise = self.hyper_background_noise_for_instance(
+            instance=instance
+        )
+
+        try:
+            fit = self.interferometer_fit_for_plane(
+                plane=plane, hyper_background_noise=hyper_background_noise
+            )
+
+            return fit.figure_of_merit
+        except (PixelizationException, InversionException, GridException) as e:
+            raise FitException from e
+
+    def associate_hyper_visibilities(
+        self, instance: af.ModelInstance
+    ) -> af.ModelInstance:
+        """
+        Takes visibilities from the last result, if there is one, and associates them with galaxies in this phase
+        where full-path galaxy names match.
+
+        If the galaxy collection has a different name then an association is not made.
+
+        e.g.
+        galaxies.lens will match with:
+            galaxies.lens
+        but not with:
+            galaxies.lens
+            galaxies.source
+
+        Parameters
+        ----------
+        instance
+            A model instance with 0 or more galaxies in its tree
+
+        Returns
+        -------
+        instance
+           The input instance with visibilities associated with galaxies where possible.
+        """
+        if self.hyper_galaxy_visibilities_path_dict is not None:
+            for galaxy_path, galaxy in instance.path_instance_tuples_for_class(
+                g.Galaxy
+            ):
+                if galaxy_path in self.hyper_galaxy_visibilities_path_dict:
+                    galaxy.hyper_model_visibilities = self.hyper_model_visibilities
+                    galaxy.hyper_galaxy_visibilities = self.hyper_galaxy_visibilities_path_dict[
+                        galaxy_path
+                    ]
+
+        return instance
+
+    def interferometer_fit_for_plane(
+        self, plane, hyper_background_noise, use_hyper_scalings=True
+    ):
+
+        return fit.FitInterferometer(
+            masked_interferometer=self.dataset,
+            plane=plane,
+            hyper_background_noise=hyper_background_noise,
+            use_hyper_scalings=use_hyper_scalings,
+            settings_pixelization=self.settings_pixelization,
+            settings_inversion=self.settings_inversion,
+        )
+
+    def visualize(self, paths: af.Paths, instance, during_analysis):
+
+        self.associate_hyper_images(instance=instance)
+        plane = self.plane_for_instance(instance=instance)
+        hyper_background_noise = self.hyper_background_noise_for_instance(
+            instance=instance
+        )
+
+        fit = self.interferometer_fit_for_plane(
+            plane=plane, hyper_background_noise=hyper_background_noise
+        )
+
+        visualizer = vis.Visualizer(visualize_path=paths.image_path)
+        visualizer.visualize_interferometer(
+            interferometer=self.interferometer.interferometer
+        )
+        visualizer.visualize_fit_interferometer(
+            fit=fit, during_analysis=during_analysis
+        )
+        if fit.inversion is not None:
+            visualizer.visualize_inversion(
+                inversion=fit.inversion, during_analysis=during_analysis
+            )
+
+        visualizer.visualize_hyper_images(
+            hyper_galaxy_image_path_dict=self.hyper_galaxy_image_path_dict,
+            hyper_model_image=self.hyper_model_image,
+            plane=plane,
+        )
+
+        if visualizer.plot_fit_no_hyper:
+            fit = self.interferometer_fit_for_plane(
+                plane=plane, hyper_background_noise=None, use_hyper_scalings=False
+            )
+
+            visualizer.visualize_fit_interferometer(
+                fit=fit, during_analysis=during_analysis, subfolders="fit_no_hyper"
+            )
