4aa82ba96c82b386746f74cb8e9702db2f5871a7
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-04-15 17:25:38 +0100

    meh

--- autogalaxy/__init__.py
+++ autogalaxy/__init__.py
@@ -6,11 +6,11 @@ from autoarray import Grid2DIrregular
 from autoarray import VectorField2DIrregular
 from autoarray import TransformerDFT
 from autoarray import pix
-from autoarray import reg 
+from autoarray import reg
 from autoarray import TransformerNUFFT
 from autoarray import SettingsInversion
 from autoarray import SettingsPixelization
-from autoarray.structures.arrays.one_d.array_1d import Array1D
+from autoarray.structures.arrays.one_d.array_1d import Array1D 
 from autoarray.structures.arrays.two_d.array_2d import Array2D
 from autoarray.structures.arrays.values import ValuesIrregular
 from autoarray.structures.grids.one_d.grid_1d import Grid1D
