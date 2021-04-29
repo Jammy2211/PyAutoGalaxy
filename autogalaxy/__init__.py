cb13c3bebf7d27e6cf188d5047a8d767c89f7cd8
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-04-29 20:00:21 +0100

    cleaned up __init__

--- autogalaxy/__init__.py
+++ autogalaxy/__init__.py
@@ -1,41 +1,50 @@
-from autoarray import Grid2DIterate
-from autoarray import Grid2DInterpolate
-from autoarray import Mask2D
-from autoarray import Grid2DIrregular
-from autoarray import Grid2DIrregular
-from autoarray import VectorField2DIrregular
-from autoarray import TransformerDFT
-from autoarray import pix
-from autoarray import reg
-from autoarray import TransformerNUFFT
-from autoarray import SettingsInversion
-from autoarray import SettingsPixelization
+from autoarray.preloads import Preloads
+from autoarray.dataset import preprocess
+from autoarray.dataset.imaging import SettingsImaging
+from autoarray.dataset.imaging import Imaging
+from autoarray.dataset.interferometer import Interferometer
+from autoarray.dataset.interferometer import SettingsInterferometer
+from autoarray.instruments import acs
+from autoarray.instruments import euclid
+from autoarray.inversion import pixelizations as pix
+from autoarray.inversion import regularization as reg
+from autoarray.inversion.inversions import SettingsInversion
+from autoarray.inversion.inversions import inversion as Inversion
+from autoarray.inversion.mappers import mapper as Mapper
+from autoarray.inversion.pixelizations import SettingsPixelization
+from autoarray.mask.mask_1d import Mask1D
+from autoarray.mask.mask_2d import Mask2D
+from autoarray.mock import fixtures
+from autoarray.operators.convolver import Convolver
+from autoarray.operators.convolver import Convolver
+from autoarray.operators.transformer import TransformerDFT
+from autoarray.operators.transformer import TransformerNUFFT
+from autoarray.layout.layout import Layout2D
 from autoarray.structures.arrays.one_d.array_1d import Array1D
 from autoarray.structures.arrays.two_d.array_2d import Array2D
 from autoarray.structures.arrays.values import ValuesIrregular
+from autoarray.structures.arrays.abstract_array import ExposureInfo
 from autoarray.structures.grids.one_d.grid_1d import Grid1D
 from autoarray.structures.grids.two_d.grid_2d import Grid2D
+from autoarray.structures.grids.two_d.grid_2d import Grid2DSparse
 from autoarray.structures.grids.two_d.grid_2d_interpolate import Grid2DInterpolate
 from autoarray.structures.grids.two_d.grid_2d_iterate import Grid2DIterate
 from autoarray.structures.grids.two_d.grid_2d_irregular import Grid2DIrregular
 from autoarray.structures.grids.two_d.grid_2d_irregular import Grid2DIrregularUniform
 from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DRectangular
 from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DVoronoi
-from autoarray.structures.vector_fields.vector_field_irregular import (
-    VectorField2DIrregular,
-)
+from autoarray.structures.vector_fields.vector_field_irregular import VectorField2DIrregular
+from autoarray.layout.region import Region1D
+from autoarray.layout.region import Region2D
 from autoarray.structures.kernel_2d import Kernel2D
 from autoarray.structures.visibilities import Visibilities
-from autoarray.dataset.imaging import Imaging, SettingsImaging
-from autoarray.dataset.interferometer import Interferometer, SettingsInterferometer
-from autoarray.operators.convolver import Convolver
+from autoarray.structures.visibilities import VisibilitiesNoiseMap
+
 from .analysis import aggregator as agg
 from . import plot
 from . import util
 from .dataset.imaging import SimulatorImaging
 from .dataset.interferometer import SimulatorInterferometer
-
-from autoconf import conf
 
 from .fit.fit_imaging import FitImaging
 from .fit.fit_interferometer import FitInterferometer
@@ -55,6 +64,8 @@ from .profiles import (
     light_and_mass_profiles as lmp,
 )
 from . import convert
+
+from autoconf import conf
 
 conf.instance.register(__file__)
 
