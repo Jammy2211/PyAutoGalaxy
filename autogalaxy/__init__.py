b1b51b196150c0912d7af29fee77b49b7343b30f
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-05-02 21:20:42 +0100

    unique tag

--- autogalaxy/__init__.py
+++ autogalaxy/__init__.py
@@ -33,7 +33,9 @@ from autoarray.structures.grids.two_d.grid_2d_irregular import Grid2DIrregular
 from autoarray.structures.grids.two_d.grid_2d_irregular import Grid2DIrregularUniform
 from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DRectangular
 from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DVoronoi
-from autoarray.structures.vector_fields.vector_field_irregular import VectorField2DIrregular
+from autoarray.structures.vector_fields.vector_field_irregular import (
+    VectorField2DIrregular,
+)
 from autoarray.layout.region import Region1D
 from autoarray.layout.region import Region2D
 from autoarray.structures.kernel_2d import Kernel2D
