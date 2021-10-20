992a51dfb7ebdfd420ad22d7b6cd1c76015de986
Richard <richardhayes777@gmail.com>
2021-10-20 15:21:19 +0100

    also test from_dict

--- test_autogalaxy/galaxy/test_dict.py
+++ test_autogalaxy/galaxy/test_dict.py
@@ -100,33 +100,59 @@ def test_pixelization_equality():
     assert Voronoi() == Voronoi()
 
 
+@pytest.fixture(
+    name="profiles_galaxy"
+)
+def make_profiles_galaxy():
+    return ag.Galaxy(
+        redshift=2.0,
+        light=ag.lp.EllChameleon(),
+        mass=ag.mp.EllDevVaucouleurs()
+    )
+
+
+@pytest.fixture(
+    name="profiles_galaxy_dict"
+)
+def make_profiles_galaxy_dict():
+    return {
+        'hyper_galaxy': None,
+        'light': {
+            'centre': (0.0, 0.0),
+            'core_radius_0': 0.01,
+            'core_radius_1': 0.05,
+            'elliptical_comps': (0.0, 0.0),
+            'intensity': 0.1,
+            'type': 'autogalaxy.profiles.light_profiles.light_profiles.EllChameleon'
+        },
+        'mass': {
+            'centre': (0.0, 0.0),
+            'effective_radius': 0.6,
+            'elliptical_comps': (0.0, 0.0),
+            'intensity': 0.1,
+            'mass_to_light_ratio': 1.0,
+            'type': 'autogalaxy.profiles.mass_profiles.stellar_mass_profiles.EllDevVaucouleurs'
+        },
+        'pixelization': None,
+        'redshift': 2.0,
+        'regularization': None,
+        'type': 'autogalaxy.galaxy.galaxy.Galaxy'
+    }
+
+
 class TestProfiles:
-    def test_dict(self):
-        galaxy = ag.Galaxy(
-            redshift=2.0,
-            light=ag.lp.EllChameleon(),
-            mass=ag.mp.EllDevVaucouleurs()
-        )
-        assert galaxy.dict() == {
-            'hyper_galaxy': None,
-            'light': {
-                'centre': (0.0, 0.0),
-                'core_radius_0': 0.01,
-                'core_radius_1': 0.05,
-                'elliptical_comps': (0.0, 0.0),
-                'intensity': 0.1,
-                'type': 'autogalaxy.profiles.light_profiles.light_profiles.EllChameleon'
-            },
-            'mass': {
-                'centre': (0.0, 0.0),
-                'effective_radius': 0.6,
-                'elliptical_comps': (0.0, 0.0),
-                'intensity': 0.1,
-                'mass_to_light_ratio': 1.0,
-                'type': 'autogalaxy.profiles.mass_profiles.stellar_mass_profiles.EllDevVaucouleurs'
-            },
-            'pixelization': None,
-            'redshift': 2.0,
-            'regularization': None,
-            'type': 'autogalaxy.galaxy.galaxy.Galaxy'
-        }
+    def test_dict(
+            self,
+            profiles_galaxy,
+            profiles_galaxy_dict
+    ):
+        assert profiles_galaxy.dict() == profiles_galaxy_dict
+
+    def test_from_dict(
+            self,
+            profiles_galaxy,
+            profiles_galaxy_dict
+    ):
+        assert Dictable.from_dict(
+            profiles_galaxy_dict
+        ) == profiles_galaxy
