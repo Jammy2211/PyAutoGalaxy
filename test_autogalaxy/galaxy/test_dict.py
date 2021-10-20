e7ee72bd94dd45b95e48ffdbda00e85a307d2060
Richard <richardhayes777@gmail.com>
2021-10-20 12:59:25 +0100

    galaxy from dict

--- test_autogalaxy/galaxy/test_dict.py
+++ test_autogalaxy/galaxy/test_dict.py
@@ -1,14 +1,42 @@
+import pytest
+
 import autogalaxy as ag
+from autogalaxy.dictable import Dictable
 
 
-def test_dict_trivial():
-    galaxy = ag.Galaxy(
+@pytest.fixture(
+    name="trivial_galaxy"
+)
+def make_trivial_galaxy():
+    return ag.Galaxy(
         redshift=1.0,
     )
-    assert galaxy.dict() == {
+
+
+@pytest.fixture(
+    name="trivial_galaxy_dict"
+)
+def make_trivial_galaxy_dict():
+    return {
         'hyper_galaxy': None,
         'pixelization': None,
         'redshift': 1.0,
         'regularization': None,
         'type': 'autogalaxy.galaxy.galaxy.Galaxy'
     }
+
+
+def test_dict_trivial(
+        trivial_galaxy,
+        trivial_galaxy_dict
+):
+    assert trivial_galaxy.dict() == trivial_galaxy_dict
+
+
+def test_from_dict(
+        trivial_galaxy,
+        trivial_galaxy_dict
+):
+    assert Dictable.from_dict(
+        trivial_galaxy_dict
+    ) == trivial_galaxy
