3b040d54eca5765c5a14cb791516e0f6db10e9f2
Richard <richardhayes777@gmail.com>
2022-08-12 10:31:00 +0100

    noqa imports

--- autogalaxy/__init__.py
+++ autogalaxy/__init__.py
@@ -1,52 +1,48 @@
-from autoarray.dataset import preprocess
-from autoarray.dataset.imaging import SettingsImaging
-from autoarray.dataset.imaging import Imaging
-from autoarray.dataset.interferometer import Interferometer
-from autoarray.dataset.interferometer import SettingsInterferometer
-from autoarray.instruments import acs
-from autoarray.instruments import euclid
-from autoarray.inversion.pixelization import mesh
-from autoarray.inversion import regularization as reg
-from autoarray.inversion.pixelization.mappers.abstract import AbstractMapper
-from autoarray.inversion.inversion.settings import SettingsInversion
-from autoarray.inversion.inversion.factory import inversion_from as Inversion
-from autoarray.inversion.inversion.factory import (
-    inversion_imaging_unpacked_from as InversionImaging,
-)
-from autoarray.inversion.inversion.factory import (
-    inversion_interferometer_unpacked_from as InversionInterferometer,
-)
-from autoarray.inversion.pixelization.pixelization import Pixelization
-from autoarray.inversion.pixelization.mappers.mapper_grids import MapperGrids
-from autoarray.inversion.pixelization.mappers.factory import mapper_from as Mapper
-from autoarray.inversion.pixelization.settings import SettingsPixelization
-from autoarray.mask.mask_1d import Mask1D
-from autoarray.mask.mask_2d import Mask2D
-from autoarray.operators.convolver import Convolver
-from autoarray.operators.convolver import Convolver
-from autoarray.operators.transformer import TransformerDFT
-from autoarray.operators.transformer import TransformerNUFFT
-from autoarray.layout.layout import Layout2D
-from autoarray.structures.arrays.uniform_1d import Array1D
-from autoarray.structures.arrays.uniform_2d import Array2D
-from autoarray.structures.values import ValuesIrregular
-from autoarray.structures.header import Header
-from autoarray.structures.grids.uniform_1d import Grid1D
-from autoarray.structures.grids.uniform_2d import Grid2D
-from autoarray.structures.grids.sparse_2d import Grid2DSparse
-from autoarray.structures.grids.iterate_2d import Grid2DIterate
-from autoarray.structures.grids.irregular_2d import Grid2DIrregular
-from autoarray.structures.grids.irregular_2d import Grid2DIrregularUniform
-from autoarray.structures.mesh.rectangular_2d import Mesh2DRectangular
-from autoarray.structures.mesh.voronoi_2d import Mesh2DVoronoi
-from autoarray.structures.mesh.delaunay_2d import Mesh2DDelaunay
-from autoarray.structures.vectors.uniform import VectorYX2D
-from autoarray.structures.vectors.irregular import VectorYX2DIrregular
-from autoarray.layout.region import Region1D
-from autoarray.layout.region import Region2D
-from autoarray.structures.arrays.kernel_2d import Kernel2D
-from autoarray.structures.visibilities import Visibilities
-from autoarray.structures.visibilities import VisibilitiesNoiseMap
+from autoarray.dataset import preprocess  # noqa
+from autoarray.dataset.imaging import SettingsImaging  # noqa
+from autoarray.dataset.imaging import Imaging  # noqa
+from autoarray.dataset.interferometer import Interferometer  # noqa
+from autoarray.dataset.interferometer import SettingsInterferometer  # noqa
+from autoarray.instruments import acs  # noqa
+from autoarray.instruments import euclid  # noqa
+from autoarray.inversion.pixelization import mesh  # noqa
+from autoarray.inversion import regularization as reg  # noqa
+from autoarray.inversion.pixelization.mappers.abstract import AbstractMapper  # noqa
+from autoarray.inversion.inversion.settings import SettingsInversion  # noqa
+from autoarray.inversion.inversion.factory import inversion_from as Inversion  # noqa
+from autoarray.inversion.inversion.factory import inversion_imaging_unpacked_from as InversionImaging # noqa
+from autoarray.inversion.inversion.factory import inversion_interferometer_unpacked_from as InversionInterferometer # noqa
+from autoarray.inversion.pixelization.pixelization import Pixelization # noqa
+from autoarray.inversion.pixelization.mappers.mapper_grids import MapperGrids # noqa
+from autoarray.inversion.pixelization.mappers.factory import mapper_from as Mapper # noqa
+from autoarray.inversion.pixelization.settings import SettingsPixelization # noqa
+from autoarray.mask.mask_1d import Mask1D # noqa
+from autoarray.mask.mask_2d import Mask2D # noqa
+from autoarray.operators.convolver import Convolver # noqa
+from autoarray.operators.convolver import Convolver # noqa
+from autoarray.operators.transformer import TransformerDFT # noqa
+from autoarray.operators.transformer import TransformerNUFFT # noqa
+from autoarray.layout.layout import Layout2D # noqa
+from autoarray.structures.arrays.uniform_1d import Array1D # noqa
+from autoarray.structures.arrays.uniform_2d import Array2D # noqa
+from autoarray.structures.values import ValuesIrregular # noqa
+from autoarray.structures.header import Header # noqa
+from autoarray.structures.grids.uniform_1d import Grid1D # noqa
+from autoarray.structures.grids.uniform_2d import Grid2D # noqa
+from autoarray.structures.grids.sparse_2d import Grid2DSparse # noqa
+from autoarray.structures.grids.iterate_2d import Grid2DIterate # noqa
+from autoarray.structures.grids.irregular_2d import Grid2DIrregular # noqa
+from autoarray.structures.grids.irregular_2d import Grid2DIrregularUniform # noqa
+from autoarray.structures.mesh.rectangular_2d import Mesh2DRectangular # noqa
+from autoarray.structures.mesh.voronoi_2d import Mesh2DVoronoi # noqa
+from autoarray.structures.mesh.delaunay_2d import Mesh2DDelaunay # noqa
+from autoarray.structures.vectors.uniform import VectorYX2D # noqa
+from autoarray.structures.vectors.irregular import VectorYX2DIrregular # noqa
+from autoarray.layout.region import Region1D # noqa
+from autoarray.layout.region import Region2D # noqa
+from autoarray.structures.arrays.kernel_2d import Kernel2D # noqa
+from autoarray.structures.visibilities import Visibilities # noqa
+from autoarray.structures.visibilities import VisibilitiesNoiseMap  # noqa
 
 from .analysis.maker import FitMaker
 from .analysis.preloads import Preloads
@@ -67,7 +63,7 @@ from .interferometer.model.analysis import AnalysisInterferometer
 from .quantity.fit_quantity import FitQuantity
 from .quantity.model.analysis import AnalysisQuantity
 from .quantity.dataset_quantity import DatasetQuantity
-from .galaxy.galaxy import Galaxy, HyperGalaxy, Redshift
+from .galaxy.galaxy import Galaxy, HyperGalaxy, Redshift, PixelizationGalaxy
 from .galaxy.stellar_dark_decomp import StellarDarkDecomp
 from .hyper import hyper_data
 from .analysis.setup import SetupHyper
@@ -92,7 +88,7 @@ from .profiles.light_profiles import (
 )
 from .profiles.light_profiles import light_profiles_snr as lp_snr
 from . import convert
-from . import mock as m
+from . import mock as m  # noqa
 from .util.shear_field import ShearYX2D
 from .util.shear_field import ShearYX2DIrregular
 from . import cosmology as cosmo
