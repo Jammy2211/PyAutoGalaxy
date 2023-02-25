85716a56c9006a438bebaec713740609799c84cb
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-02-25 19:44:11 +0000

    exponential implemented...maybe

---
+++ test_autogalaxy/profiles/light/shapelets/test_exponential.py
@@ -0,0 +1,45 @@
+from __future__ import division, print_function
+import numpy as np
+import pytest
+
+import autogalaxy as ag
+
+grid = np.array([[1.0, 1.0], [2.0, 2.0], [3.0, 3.0], [2.0, 4.0]])
+
+
+def test__image_2d_from():
+
+    shapelet = ag.lp_shapelets.ShapeletExponential(
+        n=2, m=0, centre=(0.0, 0.0), beta=1.0
+    )
+
+    image = shapelet.image_2d_from(grid=np.array([[0.0, 1.0], [0.5, 0.25]]))
+
+    assert image == pytest.approx(np.array([0.05784, 0.17962]), 1e-4)
+
+    shapelet = ag.lp_shapelets.ShapeletExponential(
+        n=2, m=0, centre=(0.2, 0.4), beta=1.0
+    )
+
+    image = shapelet.image_2d_from(grid=np.array([[0.0, 1.0], [0.5, 0.25]]))
+
+    assert image == pytest.approx(np.array([0.16136, 0.22476]), 1e-4)
+
+
+def test__elliptical__image_2d_from():
+
+    shapelet = ag.lp_shapelets.ShapeletExponentialEll(
+        n=2, m=0, centre=(0.0, 0.0), ell_comps=(0.1, 0.2), beta=1.0
+    )
+
+    image = shapelet.image_2d_from(grid=np.array([[0.0, 1.0], [0.5, 0.25]]))
+
+    assert image == pytest.approx(np.array([0.05784, 0.17961]), 1e-4)
+
+    shapelet = ag.lp_shapelets.ShapeletExponentialEll(
+        n=2, m=0, centre=(0.0, 0.0), ell_comps=(0.5, 0.7), beta=1.0
+    )
+
+    image = shapelet.image_2d_from(grid=np.array([[0.0, 1.0], [0.5, 0.25]]))
+
+    assert image == pytest.approx(np.array([0.05784, 0.17962]), 1e-4)
