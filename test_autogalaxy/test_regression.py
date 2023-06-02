fcda231ae4eaa7dd81f3ecc65cbef24c11f98c72
Richard <richard@rghsoftware.co.uk>
2023-06-02 11:43:26 +0100

    tests against paths

--- test_autogalaxy/test_regression.py
+++ test_autogalaxy/test_regression.py
@@ -1,3 +1,5 @@
+import pytest
+
 from autogalaxy import Galaxy
 from autogalaxy.abstract_fit import AbstractFitInversion
 import autofit as af
@@ -17,9 +19,12 @@ class FitInversion(AbstractFitInversion):
         return {light_profile: 1.0 for light_profile in self.light_profiles}
 
 
-def test_no_modify_state():
-    light_profile = Sersic()
+@pytest.fixture(name="light_profile")
+def make_light_profile():
+    return Sersic()
 
+
+def test_no_modify_state(light_profile):
     model_obj = af.ModelInstance(
         {
             "galaxies": af.ModelInstance(
@@ -42,3 +47,30 @@ def test_no_modify_state():
 
     assert result.galaxies.galaxy.light_profile is not light_profile
     assert model_obj.galaxies.galaxy.light_profile is light_profile
+
+
+@pytest.fixture(name="path")
+def make_path():
+    return "galaxies", 0, "light_profile"
+
+
+@pytest.fixture(name="model_obj")
+def make_model_obj(light_profile):
+    return af.ModelInstance(
+        {
+            "galaxies": [
+                Galaxy(
+                    redshift=0.5,
+                    light_profile=light_profile,
+                )
+            ]
+        }
+    )
+
+
+def test_list_galaxies(model_obj, path):
+    assert model_obj.path_instance_tuples_for_class((LightProfileLinear,))[0][0] == path
+
+
+def test_object_from_path(model_obj, path, light_profile):
+    assert model_obj.object_for_path(path) == light_profile
