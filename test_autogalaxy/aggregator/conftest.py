7d26d3d60cb20d52d3c1b594109f0484365b2d6b
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-07-12 15:23:10 +0100

    all tests fixed

--- test_autogalaxy/aggregator/conftest.py
+++ test_autogalaxy/aggregator/conftest.py
@@ -47,4 +47,5 @@ def make_samples(model):
         model=model,
         sample_list=sample_list,
         max_log_likelihood_instance=plane,
+        gaussian_tuples=[(1.0, 2.0)] * model.prior_count,
     )
