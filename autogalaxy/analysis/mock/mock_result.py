a44747f83c73f2d6f8f64faf0e76f52554852682
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-08-05 15:37:05 +0100

    simplify MockResult

--- autogalaxy/analysis/mock/mock_result.py
+++ autogalaxy/analysis/mock/mock_result.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, Dict, List
+from typing import TYPE_CHECKING, Dict
 
 if TYPE_CHECKING:
     from autogalaxy import mock
@@ -16,12 +16,8 @@ class MockResult(af.m.MockResult):
         model: af.Model = None,
         analysis: mock.MockAnalysis = None,
         search: af.mock.MockSearch = None,
-        mask: mock.MockMask = None,
-        model_image: ag.Array2D = None,
-        path_galaxy_tuples: List[(str, ag.Galaxy)] = None,
         adapt_galaxy_image_path_dict: Dict[ag.Galaxy, ag.Array2D] = None,
         adapt_model_image: ag.Array2D = None,
-        pixelization: ag.Pixelization = None,
     ):
         super().__init__(
             samples=samples,
@@ -31,15 +27,8 @@ class MockResult(af.m.MockResult):
             search=search,
         )
 
-        self.mask = mask
         self.adapt_galaxy_image_path_dict = adapt_galaxy_image_path_dict
         self.adapt_model_image = adapt_model_image
-        self.path_galaxy_tuples = path_galaxy_tuples
-        self.model_image = model_image
-        self.unmasked_model_image = model_image
-        self.pixelization = pixelization
-
-        self.max_log_likelihood_plane = ag.Plane(galaxies=[ag.Galaxy(redshift=0.5)])
 
     @property
     def last(self):
