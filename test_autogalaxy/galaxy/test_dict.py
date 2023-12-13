d221f1de5e6785af525ffa4e3ec27e687398e5e0
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-12-13 16:31:09 +0000

    fix galaxy dict

--- test_autogalaxy/galaxy/test_dict.py
+++ test_autogalaxy/galaxy/test_dict.py
@@ -38,7 +38,7 @@ def make_pixelization_galaxy():
     return PixelizationGalaxy(
         redshift=1.0,
         pixelization=ag.Pixelization(
-            mesh=Voronoi(), regularization=AdaptiveBrightness()
+            image_mesh=ag.image_mesh.Overlay(shape_overlay=(3,3)), mesh=Voronoi(), regularization=AdaptiveBrightness()
         ),
     )
 
@@ -54,6 +54,13 @@ def make_pixelization_galaxy_dict():
                 "type": "instance",
                 "class_path": "autoarray.inversion.pixelization.pixelization.Pixelization",
                 "arguments": {
+                    "image_mesh" : {
+                        "type": "instance",
+                        "class_path": "autoarray.inversion.pixelization.image_mesh.overlay.Overlay",
+                        "arguments": {
+                            "shape_overlay": (3, 3)
+                        },
+                    },
                     "mesh": {
                         "type": "instance",
                         "class_path": "autoarray.inversion.pixelization.mesh.voronoi.Voronoi",
