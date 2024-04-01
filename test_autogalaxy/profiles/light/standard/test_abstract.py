5496c3d165ef22b9ceb2ae5e3b265be5da124be0
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-04-01 17:29:00 +0100

    numeric tests on light profile decorator which now pass

--- test_autogalaxy/profiles/light/standard/test_abstract.py
+++ test_autogalaxy/profiles/light/standard/test_abstract.py
@@ -95,10 +95,7 @@ def test__decorators__grid_iterate_in__iterates_grid_correctly():
     image = light_profile.image_2d_from(grid=grid)
 
     grid_sub_2 = ag.Grid2D(values=grid, mask=mask, over_sample=ag.OverSampleUniform(sub_size=2))
-    image_sub_2 = light_profile.image_2d_from(grid=grid)
-
-    print(image)
-    print(image_sub_2)
+    image_sub_2 = light_profile.image_2d_from(grid=grid_sub_2)
 
     assert image[0] == pytest.approx(0.17481917, 1.0e-4)
     assert (image == image_sub_2).all()
@@ -112,16 +109,16 @@ def test__decorators__grid_iterate_in__iterates_grid_correctly():
 
     image = light_profile.image_2d_from(grid=grid)
 
-    over_sample = ag.OverSampleUniformFunc(mask=mask, sub_size=4)
-    grid_sub_4 = over_sample.oversampled_grid
+    grid_sub_4 = ag.Grid2D(values=grid, mask=mask, over_sample=ag.OverSampleUniform(sub_size=4))
     image_sub_4 = light_profile.image_2d_from(grid=grid_sub_4)
 
+    assert image[0] == pytest.approx(0.17754459861988386, 1.0e-4)
     assert image[0] == image_sub_4[0]
 
-    over_sample = ag.OverSampleUniformFunc(mask=mask, sub_size=8)
-    grid_sub_8 = over_sample.oversampled_grid
+    grid_sub_8 = ag.Grid2D(values=grid, mask=mask, over_sample=ag.OverSampleUniform(sub_size=8))
     image_sub_8 = light_profile.image_2d_from(grid=grid_sub_8)
 
+    assert image[4] == pytest.approx(4.173185729427679, 1.0e-4)
     assert image[4] == image_sub_8[4]
 
 
