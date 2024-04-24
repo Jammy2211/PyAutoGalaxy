a8fc04f9a248ddc983783923c84b5e0e3f3c150f
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-04-24 15:41:21 +0100

    implement more conversions docstrings

--- autogalaxy/convert.py
+++ autogalaxy/convert.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Tuple
 
 
-def ell_comps_from(axis_ratio : float, angle : float) -> Tuple[float, float]:
+def ell_comps_from(axis_ratio: float, angle: float) -> Tuple[float, float]:
     """
     Returns the elliptical components e1 and e2 of a light or mass profile from an input angle in degrees and axis
     ratio.
@@ -28,7 +28,7 @@ def ell_comps_from(axis_ratio : float, angle : float) -> Tuple[float, float]:
     return (ellip_y, ellip_x)
 
 
-def axis_ratio_and_angle_from(ell_comps : Tuple[float, float]) -> Tuple[float, float]:
+def axis_ratio_and_angle_from(ell_comps: Tuple[float, float]) -> Tuple[float, float]:
     """
     Returns the axis-ratio and position angle in degrees (-45 < angle < 135.0) from input elliptical components e1
     and e2 of a light or mass profile.
@@ -45,7 +45,7 @@ def axis_ratio_and_angle_from(ell_comps : Tuple[float, float]) -> Tuple[float, f
 
     where `fac = sqrt(ell_comps[1] ** 2 + ell_comps[0] ** 2).
 
-    Which this function returns in degrees.
+    This function returns the axis-ratio and angle in degrees.
 
     An additional check is performed which requires the angle is between -45 and 135 degrees. This ensures that
     for certain values of `ell_comps` the angle does not jump from one boundary to another (e.g. without this check
@@ -72,7 +72,7 @@ def axis_ratio_and_angle_from(ell_comps : Tuple[float, float]) -> Tuple[float, f
     return axis_ratio, angle
 
 
-def axis_ratio_from(ell_comps : Tuple[float, float]):
+def axis_ratio_from(ell_comps: Tuple[float, float]):
     """
     Returns the axis-ratio from input elliptical components e1 and e2 of a light or mass profile.
 
@@ -84,10 +84,11 @@ def axis_ratio_from(ell_comps : Tuple[float, float]):
     The axis-ratio is therefore given by:
 
     axis_ratio = (1 - fac) / (1 + fac)
+    angle = 0.5 * arctan(ell_comps[0] / ell_comps[1])
 
     where `fac = sqrt(ell_comps[1] ** 2 + ell_comps[0] ** 2).
 
-    Which this function returns.
+    This function returns the axis-ratio.
 
     Parameters
     ----------
@@ -98,7 +99,7 @@ def axis_ratio_from(ell_comps : Tuple[float, float]):
     return axis_ratio
 
 
-def angle_from(ell_comps : Tuple[float, float]) -> float:
+def angle_from(ell_comps: Tuple[float, float]) -> float:
     """
     Returns the position angle in degrees (-45 < angle < 135.0) from input elliptical components e1 and e2
     of a light or mass profile.
@@ -108,11 +109,12 @@ def angle_from(ell_comps : Tuple[float, float]) -> float:
     elliptical_component_y = ell_comps[0] = (1-q)/(1+q) * sin(2 * angle)
     elliptical_component_x = ell_comps[1] = (1-q)/(1+q) * cos(2 * angle)
 
-    The angle is therefore given by:
-
+    axis_ratio = (1 - fac) / (1 + fac)
     angle = 0.5 * arctan(ell_comps[0] / ell_comps[1])
 
-    Which this function returns in degrees.
+    where `fac = sqrt(ell_comps[1] ** 2 + ell_comps[0] ** 2).
+
+    This function returns the angle in degrees.
 
     An additional check is performed which requires the angle is between -45 and 135 degrees. This ensures that
     for certain values of `ell_comps` the angle does not jump from one boundary to another (e.g. without this check
@@ -132,9 +134,26 @@ def angle_from(ell_comps : Tuple[float, float]) -> float:
 
 def shear_gamma_1_2_from(magnitude: float, angle: float) -> Tuple[float, float]:
     """
-    :param angle: angel
-    :param magnitude: ellipticity
-    :return:
+    Returns the shear gamma 1 and gamma 2 values an input shear magnitude and angle in degrees.
+
+    The gamma 1 and gamma 2 components of a shear are given by:
+
+    gamma_1 = magnitude * np.cos(2 * angle * np.pi / 180.0)
+    gamma_2 = magnitude * np.sin(2 * angle * np.pi / 180.0)
+
+    Which are the values this function returns.
+
+    Converting from gamma 1 and gamma 2 to magnitude and angle is given by:
+
+    magnitude = np.sqrt(gamma_1**2 + gamma_2**2)
+    angle = np.arctan2(gamma_2, gamma_1) / 2 * 180.0 / np.pi
+
+    Parameters
+    ----------
+    axis_ratio
+        Ratio of light profiles ellipse's minor and major axes (b/a).
+    angle
+        Rotation angle of light profile counter-clockwise from positive x-axis.
     """
     gamma_1 = magnitude * np.cos(2 * angle * np.pi / 180.0)
     gamma_2 = magnitude * np.sin(2 * angle * np.pi / 180.0)
@@ -145,9 +164,32 @@ def shear_magnitude_and_angle_from(
     gamma_1: float, gamma_2: float
 ) -> Tuple[float, float]:
     """
-    :param e1: ellipticity component
-    :param e2: ellipticity component
-    :return: angle and abs value of ellipticity
+    Returns the shear magnitude and angle in degrees from input shear gamma 1 and gamma 2 values.
+
+    The gamma 1 and gamma 2 components of a shear are given by:
+
+    gamma_1 = magnitude * np.cos(2 * angle * np.pi / 180.0)
+    gamma_2 = magnitude * np.sin(2 * angle * np.pi / 180.0)
+
+    Converting from gamma 1 and gamma 2 to magnitude and angle is given by:
+
+    magnitude = np.sqrt(gamma_1**2 + gamma_2**2)
+    angle = np.arctan2(gamma_2, gamma_1) / 2 * 180.0 / np.pi
+
+    Which are the values this function returns.
+
+    Additional checks are performed which requires the angle is between -45 and 135 degrees. This ensures that
+    for certain values of `gamma_1` and `gamma_2` the angle does not jump from one boundary to another (e.g. without
+    this check certain values of `gamma_1` and `gamma_2` return -1.0 degrees and others 179.0 degrees). This ensures
+    that when error estimates are computed from samples of a lens model via marginalization, the calculation is not
+    biased by the angle jumping between these two values.
+
+    Parameters
+    ----------
+    gamma_1
+        The gamma 1 component of the shear.
+    gamma_2
+        The gamma 2 component of the shear.
     """
     angle = np.arctan2(gamma_2, gamma_1) / 2 * 180.0 / np.pi
     magnitude = np.sqrt(gamma_1**2 + gamma_2**2)
@@ -155,18 +197,34 @@ def shear_magnitude_and_angle_from(
     if angle < 0:
         angle += 180.0
 
-    if abs(angle - 90) > 45:
-        if angle > 90:
-            angle -= 180
+    if abs(angle - 90) > 45 and angle > 90:
+        angle -= 180
 
     return magnitude, angle
 
 
 def shear_magnitude_from(gamma_1: float, gamma_2: float) -> float:
     """
-    :param e1: ellipticity component
-    :param e2: ellipticity component
-    :return: angle and abs value of ellipticity
+    Returns the shear magnitude and angle in degrees from input shear gamma 1 and gamma 2 values.
+
+    The gamma 1 and gamma 2 components of a shear are given by:
+
+    gamma_1 = magnitude * np.cos(2 * angle * np.pi / 180.0)
+    gamma_2 = magnitude * np.sin(2 * angle * np.pi / 180.0)
+
+    Converting from gamma 1 and gamma 2 to magnitude and angle is given by:
+
+    magnitude = np.sqrt(gamma_1**2 + gamma_2**2)
+    angle = np.arctan2(gamma_2, gamma_1) / 2 * 180.0 / np.pi
+
+    The magnitude value is what this function returns.
+
+    Parameters
+    ----------
+    gamma_1
+        The gamma 1 component of the shear.
+    gamma_2
+        The gamma 2 component of the shear.
     """
     magnitude, angle = shear_magnitude_and_angle_from(gamma_1=gamma_1, gamma_2=gamma_2)
     return magnitude
@@ -174,9 +232,26 @@ def shear_magnitude_from(gamma_1: float, gamma_2: float) -> float:
 
 def shear_angle_from(gamma_1: float, gamma_2: float) -> float:
     """
-    :param e1: ellipticity component
-    :param e2: ellipticity component
-    :return: angle and abs value of ellipticity
+    Returns the shear magnitude and angle in degrees from input shear gamma 1 and gamma 2 values.
+
+    The gamma 1 and gamma 2 components of a shear are given by:
+
+    gamma_1 = magnitude * np.cos(2 * angle * np.pi / 180.0)
+    gamma_2 = magnitude * np.sin(2 * angle * np.pi / 180.0)
+
+    Converting from gamma 1 and gamma 2 to magnitude and angle is given by:
+
+    magnitude = np.sqrt(gamma_1**2 + gamma_2**2)
+    angle = np.arctan2(gamma_2, gamma_1) / 2 * 180.0 / np.pi
+
+    The angle value is what this function returns.
+
+    Parameters
+    ----------
+    gamma_1
+        The gamma 1 component of the shear.
+    gamma_2
+        The gamma 2 component of the shear.
     """
     magnitude, angle = shear_magnitude_and_angle_from(gamma_1=gamma_1, gamma_2=gamma_2)
     return angle
--- test_autogalaxy/test_convert.py
+++ test_autogalaxy/test_convert.py
@@ -2,104 +2,78 @@ import pytest
 
 import autogalaxy as ag
 
-def test__ell_comps_from():
 
-    ell_comps = ag.convert.ell_comps_from(
-        axis_ratio=0.00050025012, angle=0.0
-    )
+def test__ell_comps_from():
+    ell_comps = ag.convert.ell_comps_from(axis_ratio=0.00050025012, angle=0.0)
 
     assert ell_comps == pytest.approx((0.0, 0.999), 1.0e-4)
 
+
 def test__axis_ratio_and_angle_from():
-    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(
-        ell_comps=(0.0, 1.0)
-    )
+    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(ell_comps=(0.0, 1.0))
 
     assert axis_ratio == pytest.approx(0.00050025012, 1.0e-4)
     assert angle == pytest.approx(0.0, 1.0e-4)
 
-    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(
-        ell_comps=(1.0, 0.0)
-    )
+    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(ell_comps=(1.0, 0.0))
 
     assert axis_ratio == pytest.approx(0.00050025012, 1.0e-4)
     assert angle == pytest.approx(45.0, 1.0e-4)
 
-    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(
-        ell_comps=(0.0, -1.0)
-    )
+    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(ell_comps=(0.0, -1.0))
 
     assert axis_ratio == pytest.approx(0.00050025012, 1.0e-4)
     assert angle == pytest.approx(90.0, 1.0e-4)
 
-    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(
-        ell_comps=(-1.0, 0.0)
-    )
-    
+    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(ell_comps=(-1.0, 0.0))
+
     assert axis_ratio == pytest.approx(0.00050025012, 1.0e-4)
     assert angle == pytest.approx(-45.0, 1.0e-4)
 
-    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(
-        ell_comps=(-1.0, -1.0)
-    )
+    axis_ratio, angle = ag.convert.axis_ratio_and_angle_from(ell_comps=(-1.0, -1.0))
 
     assert axis_ratio == pytest.approx(0.00050025012, 1.0e-4)
     assert angle == pytest.approx(112.5, 1.0e-4)
 
 
 def test__shear_gamma_1_2_from():
-
-    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(
-        magnitude=0.05, angle=0.0
-    )
+    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(magnitude=0.05, angle=0.0)
 
     assert gamma_1 == pytest.approx(0.05, 1.0e-4)
     assert gamma_2 == pytest.approx(0.0, 1.0e-4)
 
-    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(
-        magnitude=0.05, angle=45.0
-    )
+    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(magnitude=0.05, angle=45.0)
 
     assert gamma_1 == pytest.approx(0.0, 1.0e-4)
     assert gamma_2 == pytest.approx(0.05, 1.0e-4)
 
-    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(
-        magnitude=0.05, angle=90.0
-    )
+    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(magnitude=0.05, angle=90.0)
 
     assert gamma_1 == pytest.approx(-0.05, 1.0e-4)
     assert gamma_2 == pytest.approx(0.0, 1.0e-4)
 
-    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(
-        magnitude=0.05, angle=135.0
-    )
+    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(magnitude=0.05, angle=135.0)
 
     assert gamma_1 == pytest.approx(0.0, 1.0e-4)
     assert gamma_2 == pytest.approx(-0.05, 1.0e-4)
 
-    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(
-        magnitude=0.05, angle=180.0
-    )
+    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(magnitude=0.05, angle=180.0)
 
     assert gamma_1 == pytest.approx(0.05, 1.0e-4)
     assert gamma_2 == pytest.approx(0.0, 1.0e-4)
 
-    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(
-        magnitude=0.05, angle=225.0
-    )
+    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(magnitude=0.05, angle=225.0)
 
     assert gamma_1 == pytest.approx(0.0, 1.0e-4)
     assert gamma_2 == pytest.approx(0.05, 1.0e-4)
 
-    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(
-        magnitude=0.05, angle=-45.0
-    )
+    gamma_1, gamma_2 = ag.convert.shear_gamma_1_2_from(magnitude=0.05, angle=-45.0)
 
     assert gamma_1 == pytest.approx(0.0, 1.0e-4)
     assert gamma_2 == pytest.approx(-0.05, 1.0e-4)
 
-def test__shear_magnitude_and_angle_from():
 
+def test__shear_magnitude_and_angle_from():
     magnitude, angle = ag.convert.shear_magnitude_and_angle_from(
         gamma_1=0.05, gamma_2=0.0
     )
