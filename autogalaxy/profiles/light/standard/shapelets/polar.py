6084512c4fc0d00bd2a13f3e33674e12a3472e71
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-05-15 16:00:25 +0100

    polar refactor

--- autogalaxy/profiles/light/standard/shapelets/polar.py
+++ autogalaxy/profiles/light/standard/shapelets/polar.py
@@ -81,32 +81,54 @@ class ShapeletPolar(AbstractShapelet):
             The image of the Polar Shapelet evaluated at every (y,x) coordinate on the transformed grid.
         """
 
-        radial = (grid[:, 0] ** 2 + grid[:, 1] ** 2) / self.beta**2.0
-        theta = np.arctan(grid[:, 1] / grid[:, 0])
+        # radial = (grid[:, 0] ** 2 + grid[:, 1] ** 2) / self.beta**2.0
+        # theta = np.arctan(grid[:, 1] / grid[:, 0])
 
         laguerre = genlaguerre(n=(self.n - np.abs(self.m)) / 2.0, alpha=np.abs(self.m))
 
-        shapelet = laguerre(radial)
+        # shapelet = laguerre(radial)
+        #
+        # const = (
+        #     ((-1) ** ((self.n - np.abs(self.m)) / 2))
+        #     * np.sqrt(
+        #         factorial((self.n - np.abs(self.m)) / 2)
+        #         / factorial((self.n + np.abs(self.m)) / 2)
+        #     )
+        #     / self.beta
+        #     / np.sqrt(np.pi)
+        # )
+        # gauss = np.exp(-radial / 2.0)
+        #
+        # return np.abs(
+        #     const
+        #     * radial ** (np.abs(self.m / 2.0))
+        #     * shapelet
+        #     * gauss
+        #     * np.exp(0.0 + 1j * -self.m * theta)
+        # )
 
         const = (
-            ((-1) ** ((self.n - np.abs(self.m)) / 2))
-            * np.sqrt(
-                factorial((self.n - np.abs(self.m)) / 2)
-                / factorial((self.n + np.abs(self.m)) / 2)
-            )
-            / self.beta
-            / np.sqrt(np.pi)
+                ((-1) ** ((self.n - np.abs(self.m)) // 2))
+                * np.sqrt(
+            factorial((self.n - np.abs(self.m)) // 2)
+            / factorial((self.n + np.abs(self.m)) // 2)
         )
-        gauss = np.exp(-radial / 2.0)
-
-        return np.abs(
-            const
-            * radial ** (np.abs(self.m / 2.0))
-            * shapelet
-            * gauss
-            * np.exp(0.0 + 1j * -self.m * theta)
+                / self.beta
+                / np.sqrt(np.pi)
         )
 
+        rsq = (grid[:,0] ** 2 + grid[:,1] ** 2) / self.beta ** 2
+        theta = np.arctan2(grid[:, 1], grid[:, 0])
+        radial = rsq ** (abs(self.m / 2.)) * np.exp(-rsq / 2.) * laguerre(rsq)
+
+        if self.m == 0:
+            azimuthal = 1
+        elif self.m > 0:
+            azimuthal = np.sin((-1) * self.m * theta)
+        else:
+            azimuthal = np.cos((-1) * self.m * theta)
+
+        return  const * radial * azimuthal
 
 class ShapeletPolarSph(ShapeletPolar):
     def __init__(
