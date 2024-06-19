fc8980570e0665f12ceb1ee39b297756f3c31fee
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-06-19 09:44:09 +0100

    basis filters mass profiles correctly

--- autogalaxy/profiles/light/basis.py
+++ autogalaxy/profiles/light/basis.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Type, Union
 
 import autoarray as aa
 
@@ -23,6 +23,35 @@ class Basis(LightProfile, MassProfile):
         self.light_profile_list = light_profile_list
         self.regularization = regularization
 
+    @property
+    def mass_list(self) -> List:
+        """
+        Returns a list of objects in the galaxy which are an instance of the input `cls`.
+
+        The optional `cls_filtered` input removes classes of an input instance type.
+
+        For example:
+
+        - If the input is `cls=ag.LightProfile`, a list containing all light profiles in the galaxy is returned.
+
+        - If `cls=ag.LightProfile` and `cls_filtered=ag.LightProfileLinear`, a list of all light profiles
+          excluding those which are linear light profiles will be returned.
+
+        Parameters
+        ----------
+        cls
+            The type of class that a list of instances of this class in the galaxy are returned for.
+        cls_filtered
+            A class type which is filtered and removed from the class list.
+
+        Returns
+        -------
+            The list of objects in the galaxy that inherit from input `cls`.
+        """
+        return aa.util.misc.cls_list_from(
+            values=self.light_profile_list, cls=MassProfile
+        )
+
     def image_2d_from(
         self, grid: aa.type.Grid2DLike, operated_only: Optional[bool] = None, **kwargs
     ) -> aa.Array2D:
@@ -41,17 +70,25 @@ class Basis(LightProfile, MassProfile):
     def convergence_2d_from(
         self, grid: aa.type.Grid2DLike, **kwargs
     ) -> aa.Array2D:
-        return sum([mass.convergence_2d_from(grid=grid) for mass in self.light_profile_list])
+
+        if len(self.mass_list) > 0:
+            return sum([mass.convergence_2d_from(grid=grid) for mass in self.mass_list])
+        return np.zeros((grid.shape[0],))
 
     def potential_2d_from(
         self, grid: aa.type.Grid2DLike, **kwargs
     ) -> aa.Array2D:
-        return sum([mass.potential_2d_from(grid=grid) for mass in self.light_profile_list])
+        if len(self.mass_list) > 0:
+            return sum([mass.potential_2d_from(grid=grid) for mass in self.light_profile_list])
+        return np.zeros((grid.shape[0],))
+
 
     def deflections_yx_2d_from(
         self, grid: aa.type.Grid2DLike, **kwargs
     ) -> aa.Array2D:
-        return sum([mass.deflections_yx_2d_from(grid=grid) for mass in self.light_profile_list])
+        if len(self.mass_list) > 0:
+            return sum([mass.deflections_yx_2d_from(grid=grid) for mass in self.light_profile_list])
+        return np.zeros((grid.shape[0], 2))
 
     def lp_instance_from(self, linear_light_profile_intensity_dict: Dict):
         light_profile_list = []
