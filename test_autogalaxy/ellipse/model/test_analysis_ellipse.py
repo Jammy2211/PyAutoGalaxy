a3b8b9e35491a2084e113e546a65d768e1f4eeaf
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-07-26 11:51:06 +0100

    test on fit pases

--- test_autogalaxy/ellipse/model/test_analysis_ellipse.py
+++ test_autogalaxy/ellipse/model/test_analysis_ellipse.py
@@ -9,10 +9,7 @@ directory = path.dirname(path.realpath(__file__))
 
 
 def test__make_result__result_imaging_is_returned(masked_imaging_7x7):
-
-    ellipse_list = af.Collection(
-        af.Model(ag.Ellipse) for _ in range(2)
-    )
+    ellipse_list = af.Collection(af.Model(ag.Ellipse) for _ in range(2))
 
     ellipse_list[0].major_axis = 1.0
     ellipse_list[1].major_axis = 2.0
@@ -28,18 +25,25 @@ def test__make_result__result_imaging_is_returned(masked_imaging_7x7):
     assert isinstance(result, ResultEllipse)
 
 
-# def test__figure_of_merit(
-#     masked_imaging_7x7,
-# ):
-#     ellipse_list = [af.Model(ag.Ellipse(major_axis=1.0)), af.Model(ag.Ellipse(major_axis=2.0))]
-#
-#     model = af.Collection(ellipses=ellipse_list)
-#
-#     analysis = ag.AnalysisEllipse(dataset=masked_imaging_7x7)
-#
-#     instance = model.instance_from_unit_vector([])
-#     fit_figure_of_merit = analysis.log_likelihood_function(instance=instance)
-#
-#     fit_list = [ag.FitEllipse(dataset=masked_imaging_7x7, ellipse=ellipse) for ellipse in instance.ellipses]
-#
-#     assert fit_list[0].log_likelihood + fit_list[1].log_likelihood == fit_figure_of_merit
+def test__figure_of_merit(
+    masked_imaging_7x7,
+):
+    ellipse_list = af.Collection(af.Model(ag.Ellipse) for _ in range(2))
+
+    ellipse_list[0].major_axis = 1.0
+    ellipse_list[1].major_axis = 2.0
+
+    model = af.Collection(ellipses=ellipse_list)
+    analysis = ag.AnalysisEllipse(dataset=masked_imaging_7x7)
+
+    instance = model.instance_from_prior_medians()
+    fit_figure_of_merit = analysis.log_likelihood_function(instance=instance)
+
+    fit_list = [
+        ag.FitEllipse(dataset=masked_imaging_7x7, ellipse=ellipse)
+        for ellipse in instance.ellipses
+    ]
+
+    assert (
+        fit_list[0].log_likelihood + fit_list[1].log_likelihood == fit_figure_of_merit
+    )
