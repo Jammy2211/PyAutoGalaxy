34a4ae2de16bf8ccff09b1f32615926e1de411bd
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-08-06 20:12:52 +0100

    Remove POintSourceChi

--- autogalaxy/quantity/fit_quantity.py
+++ autogalaxy/quantity/fit_quantity.py
@@ -72,7 +72,7 @@ class FitQuantity(aa.FitImaging):
         """
         if isinstance(self.data, aa.VectorYX2D):
             return FitQuantity(
-                dataset=self.dataset.y,
+                dataset=self.dataset.model_curve,
                 light_mass_obj=self.light_mass_obj,
                 func_str=self.func_str,
                 model_data_manual=self.model_data.y,
