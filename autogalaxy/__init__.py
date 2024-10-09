bf466da94d41d8ba05069d1f80d6c014d5ffc75a
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-10-09 15:19:43 +0100

    black

--- autogalaxy/__init__.py
+++ autogalaxy/__init__.py
@@ -4,6 +4,7 @@ from autoarray.dataset.imaging.dataset import Imaging  # noqa
 from autoarray.dataset.interferometer.dataset import Interferometer  # noqa
 from autoarray.dataset.dataset_model import DatasetModel
 from autoarray.dataset.over_sampling import OverSamplingDataset
+from autoarray.inversion.inversion.mapper_valued import MapperValued
 from autoarray.inversion.pixelization import mesh  # noqa
 from autoarray.inversion import regularization as reg  # noqa
 from autoarray.inversion.pixelization import image_mesh
