6b0ac09d10db0ee1da87234df643679020d21557
James Nightingale <james.w.nightingale@durham.ac.uk>
2025-02-25 19:36:05 +0000

    imports

--- autogalaxy/profiles/mass/dark/__init__.py
+++ autogalaxy/profiles/mass/dark/__init__.py
@@ -1,6 +1,7 @@
 from .gnfw import gNFW, gNFWSph
 from .gnfw_mcr import gNFWMCRLudlow
 from .gnfw_virial_mass_conc import gNFWVirialMassConcSph
+from .gnfw_virial_mass_gnfw_conc import gNFWVirialMassgNFWConcSph
 from .nfw import NFW, NFWSph
 from .nfw_mcr import NFWMCRLudlowSph, NFWMCRDuffySph, NFWMCRLudlow
 from .nfw_mcr_scatter import NFWMCRScatterLudlow, NFWMCRScatterLudlowSph
