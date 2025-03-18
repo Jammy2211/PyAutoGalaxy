4fbd00683a9180144d6fb0ab5b7542b9d293486f
James Nightingale <james.w.nightingale@durham.ac.uk>
2025-03-18 19:44:55 +0000

    extend aggregator examples

--- autogalaxy/aggregator/__init__.py
+++ autogalaxy/aggregator/__init__.py
@@ -8,4 +8,4 @@ from autogalaxy.aggregator.ellipse.multipoles import MultipolesAgg
 from autogalaxy.aggregator.ellipse.fit_ellipse import FitEllipseAgg
 from autogalaxy.aggregator.subplot import SubplotDataset as subplot_dataset
 from autogalaxy.aggregator.subplot import SubplotFit as subplot_fit
-from autogalaxy.aggregator.subplot import FitFITS as fits_fit
+from autogalaxy.aggregator.subplot import FITSFit as fits_fit
