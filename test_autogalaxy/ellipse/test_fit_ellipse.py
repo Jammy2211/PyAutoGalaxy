0ed099ecd621a1d3e5ef5e0c75dc55e595010848
samlange04 <80762641+samlange04@users.noreply.github.com>
2025-10-16 12:35:13 +0800

    Update test_fit_ellipse.py
    Update ellipse multipole to new angle system

--- test_autogalaxy/ellipse/test_fit_ellipse.py
+++ test_autogalaxy/ellipse/test_fit_ellipse.py
@@ -63,7 +63,7 @@ def test___points_from_major_axis__multipole(imaging_lh):
         dataset=imaging_lh, ellipse=ellipse_0, multipole_list=[multipole]
     )
 
-    assert fit._points_from_major_axis[1, 0] == pytest.approx(-0.542453, 1.0e-4)
+    assert fit._points_from_major_axis[1, 0] == pytest.approx(-0.119588, 1.0e-4)
     assert fit._points_from_major_axis[1, 1] == pytest.approx(-0.038278334, 1.0e-4)
 
 
@@ -197,3 +197,4 @@ def test__log_likelihood(imaging_lh, imaging_lh_masked):
     fit = ag.FitEllipse(dataset=imaging_lh_masked, ellipse=ellipse_0)
 
     assert fit.log_likelihood == pytest.approx(-0.169821080058, 1.0e-4)
+
