deb84030d891b9a4c2c82a791c14f9b1fd057361
Jammy2211 <JNightingale2211@gmail.com>
2025-11-09 08:11:45 +0000

    fixed aggregator typing isue with voer saple array

--- autogalaxy/imaging/model/plotter_interface.py
+++ autogalaxy/imaging/model/plotter_interface.py
@@ -117,8 +117,8 @@ class PlotterInterfaceImaging(PlotterInterface):
                 dataset.data.native_for_fits,
                 dataset.noise_map.native_for_fits,
                 dataset.psf.native_for_fits,
-                dataset.grids.lp.over_sample_size.native_for_fits,
-                dataset.grids.pixelization.over_sample_size.native_for_fits,
+                dataset.grids.lp.over_sample_size.native_for_fits.astype("float"),
+                dataset.grids.pixelization.over_sample_size.native_for_fits.astype("float"),
             ]
 
             hdu_list = hdu_list_for_output_from(
