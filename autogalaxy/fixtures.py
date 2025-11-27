f4fe004e4a8972b7ebd5df3443bc1942ccaca98e
Jammy2211 <JNightingale2211@gmail.com>
2025-11-27 17:11:59 +0000

    fix aggreegaor tests

--- autogalaxy/fixtures.py
+++ autogalaxy/fixtures.py
@@ -261,15 +261,18 @@ def make_samples_summary_with_result():
 def make_analysis_imaging_7x7():
     analysis = ag.AnalysisImaging(
         dataset=make_masked_imaging_7x7(),
+        adapt_images=make_adapt_images_7x7(),
         use_jax=False,
     )
-    analysis._adapt_images = make_adapt_images_7x7()
     return analysis
 
 
 def make_analysis_interferometer_7():
-    analysis = ag.AnalysisInterferometer(dataset=make_interferometer_7(), use_jax=False)
-    analysis._adapt_images = make_adapt_images_7x7()
+    analysis = ag.AnalysisInterferometer(
+        dataset=make_interferometer_7(),
+        adapt_images=make_adapt_images_7x7(),
+        use_jax=False
+    )
     return analysis
 
 
