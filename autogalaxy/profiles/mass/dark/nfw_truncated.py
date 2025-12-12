cdea5ae2def1b22e59deb7a8247684816713de18
Hengkai_Pmo_StarB <hkding@pmo.ac.cn>
2025-12-13 00:07:39 +0800

    Merge branch 'main' into fix/ShapeletPolar_dPIEkappa

--- autogalaxy/profiles/mass/dark/nfw_truncated.py
+++ autogalaxy/profiles/mass/dark/nfw_truncated.py
@@ -39,7 +39,8 @@ class NFWTruncatedSph(AbstractgNFW):
         """
 
         eta = xp.multiply(
-            1.0 / self.scale_radius, self.radial_grid_from(grid=grid, xp=xp, **kwargs).array
+            1.0 / self.scale_radius,
+            self.radial_grid_from(grid=grid, xp=xp, **kwargs).array,
         )
 
         deflection_grid = xp.multiply(
