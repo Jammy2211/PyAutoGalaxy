3d1f3a36486bd01f92b3d033c05252a701ecbf72
NiekWielders <c4072114@newcastle.ac.uk>
2026-02-19 11:38:11 +0000

    added mge.py instead of changing mge_numpy and jaxified eta

---
+++ autogalaxy/profiles/mass/abstract/mge.py
@@ -0,0 +1,187 @@
+import numpy as np
+
+
+class MassProfileMGE:
+    """
+    This class speeds up deflection angle calculations of certain mass profiles by decompositing them into many
+    Gaussians.
+
+    This follows the method of Shajib 2019 - https://academic.oup.com/mnras/article/488/1/1387/5526256
+    """
+
+    def __init__(self):
+        self.count = 0
+        self.sigma_calc = 0
+        self.z = 0
+        self.zq = 0
+        self.expv = 0
+
+    @staticmethod
+    def zeta_from(grid, amps, sigmas, axis_ratio):
+        """
+        The key part to compute the deflection angle of each Gaussian.
+        Because of my optimization, there are blocks looking weird and indirect. What I'm doing here
+        is trying to avoid big matrix operation to save time.
+        I think there are still spaces we can optimize.
+
+        It seems when using w_f_approx, it gives some errors if y < 0. So when computing for places
+        where y < 0, we first compute the value at - y, and then change its sign.
+        """
+
+        output_grid_final = np.zeros(grid.shape[0], dtype="complex128")
+
+        q2 = axis_ratio**2.0
+
+        scale_factor = axis_ratio / (sigmas[0] * np.sqrt(2.0 * (1.0 - q2)))
+
+        xs = np.array((grid.array[:, 1] * scale_factor).copy())
+        ys = np.array((grid.array[:, 0] * scale_factor).copy())
+
+        ys_minus = ys < 0.0
+        ys[ys_minus] *= -1
+        z = xs + 1j * ys
+        zq = axis_ratio * xs + 1j * ys / axis_ratio
+
+        expv = -(xs**2.0) * (1.0 - q2) - ys**2.0 * (1.0 / q2 - 1.0)
+
+        for i in range(len(sigmas)):
+            if i > 0:
+                z /= sigmas[i] / sigmas[i - 1]
+                zq /= sigmas[i] / sigmas[i - 1]
+                expv /= (sigmas[i] / sigmas[i - 1]) ** 2.0
+
+            output_grid = -1j * (w_f_approx(z) - np.exp(expv) * w_f_approx(zq))
+
+            output_grid[ys_minus] = np.conj(output_grid[ys_minus])
+
+            output_grid_final += (amps[i] * sigmas[i]) * output_grid
+
+        return output_grid_final
+
+    @staticmethod
+    def kesi(p, xp=np):
+        """
+        see Eq.(6) of Shajib 2019 1906.08263
+        """
+        n_list = xp.arange(0, 2 * p + 1, 1)
+        return (2.0 * p * xp.log(10) / 3.0 + 2.0 * xp.pi * n_list * 1j) ** (0.5)
+
+    @staticmethod
+    def eta(p, xp=np):
+        """
+        see Eq.(6) of Shajib 2019 1906.00263
+        """
+        i = xp.arange(1, p, 1)
+        kesi_last = 1 / 2 ** p
+        k = kesi_last + xp.cumsum(xp.cumprod((p + 1 - i) / i) * kesi_last)
+
+        kesi_list = xp.hstack(
+            [xp.array([0.5]), xp.ones(p), k[::-1], xp.array([kesi_last])]
+        )
+        coef = (-1) ** xp.arange(0, 2 * p + 1, 1)
+        eta_const = 2.0 * xp.sqrt(2.0 * xp.pi) * 10 ** (p / 3.0)
+        eta_list = coef * eta_const * kesi_list
+
+        return eta_list
+
+    def decompose_convergence_via_mge(self):
+        raise NotImplementedError()
+
+    def _decompose_convergence_via_mge(
+        self, func, radii_min, radii_max, func_terms=28, func_gaussians=20
+    ):
+        """
+
+        Parameters
+        ----------
+        func : func
+            The function representing the profile that is decomposed into Gaussians.
+        normalization
+            A normalization factor tyh
+        func_terms
+            The number of terms used to approximate the input func.
+        func_gaussians
+            The number of Gaussians used to represent the input func.
+
+        Returns
+        -------
+        """
+
+        kesis = self.kesi(func_terms)  # kesi in Eq.(6) of 1906.08263
+        etas = self.eta(func_terms)  # eta in Eqr.(6) of 1906.08263
+
+        def f(sigma):
+            """Eq.(5) of 1906.08263"""
+            return np.sum(etas * np.real(target_function(sigma * kesis)))
+
+        # sigma is sampled from logspace between these radii.
+
+        log_sigmas = np.linspace(np.log(radii_min), np.log(radii_max), func_gaussians)
+        d_log_sigma = log_sigmas[1] - log_sigmas[0]
+        sigma_list = np.exp(log_sigmas)
+
+        amplitude_list = np.zeros(func_gaussians)
+
+        for i in range(func_gaussians):
+            f_sigma = np.sum(etas * np.real(func(sigma_list[i] * kesis)))
+            if (i == -1) or (i == (func_gaussians - 1)):
+                amplitude_list[i] = 0.5 * f_sigma * d_log_sigma / np.sqrt(2.0 * np.pi)
+            else:
+                amplitude_list[i] = f_sigma * d_log_sigma / np.sqrt(2.0 * np.pi)
+
+        return amplitude_list, sigma_list
+
+    def convergence_2d_via_mge_from(self, grid_radii):
+        raise NotImplementedError()
+
+    def _convergence_2d_via_mge_from(self, grid_radii, **kwargs):
+        """Calculate the projected convergence at a given set of arc-second gridded coordinates.
+
+        Parameters
+        ----------
+        grid
+            The grid of (y,x) arc-second coordinates the convergence is computed on.
+
+        """
+
+        self.count = 0
+        self.sigma_calc = 0
+        self.z = 0
+        self.zq = 0
+        self.expv = 0
+
+        amps, sigmas = self.decompose_convergence_via_mge()
+
+        convergence = 0.0
+
+        for i in range(len(sigmas)):
+            convergence += self.convergence_func_gaussian(
+                grid_radii=grid_radii.array, sigma=sigmas[i], intensity=amps[i]
+            )
+        return convergence
+
+    def convergence_func_gaussian(self, grid_radii, sigma, intensity):
+        return np.multiply(
+            intensity, np.exp(-0.5 * np.square(np.divide(grid_radii, sigma)))
+        )
+
+    def _deflections_2d_via_mge_from(
+        self, grid, sigmas_factor=1.0, func_terms=None, func_gaussians=None
+    ):
+        axis_ratio = np.array(self.axis_ratio())
+
+        if axis_ratio > 0.9999:
+            axis_ratio = 0.9999
+
+        amps, sigmas = self.decompose_convergence_via_mge()
+        sigmas *= sigmas_factor
+
+        angle = self.zeta_from(
+            grid=grid, amps=amps, sigmas=sigmas, axis_ratio=axis_ratio
+        )
+
+        angle *= np.sqrt((2.0 * np.pi) / (1.0 - axis_ratio**2.0))
+
+        return self.rotated_grid_from_reference_frame_from(
+            np.vstack((-angle.imag, angle.real)).T
+        )
