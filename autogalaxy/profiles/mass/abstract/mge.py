b03dacd4ea0bfe9861a663c80dd788631f37a0e8
NiekWielders <c4072114@newcastle.ac.uk>
2026-02-26 15:28:07 +0000

    prevent overflow error

--- autogalaxy/profiles/mass/abstract/mge.py
+++ autogalaxy/profiles/mass/abstract/mge.py
@@ -107,7 +107,7 @@ class MGEDecomposer:
         for v in V5:
             s = v + z2 * s
 
-        w5 = xp.exp(-z2) + 1j * z * t / s
+        w5 = xp.exp(xp.clip(-z2, None, 700.0)) + 1j * z * t / s #clip prevents overflow error
 
         # ---------- Region 6 ----------
         U6 = xp.asarray(
